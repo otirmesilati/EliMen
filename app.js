13aad06630638dbc4448aefcda2f89c7fa5314b0
Otir <otirmesi@gmail.com>
2024-12-13 11:19:59 +0200

    Assets

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get("/test", (req, res) => {
   res.send("Hi");
